022351386dcfc0661ac7eabf122e43aec48f6feb
yb <wangyibu123@gmail.com>
2025-05-19 00:03:04 +0900

    update code

--- go.mod
+++ go.mod
@@ -13,6 +13,8 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/markbates/goth v1.81.0
 	github.com/spf13/viper v1.20.1
+	github.com/swaggo/files v1.0.1
+	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.16.4
 	github.com/testcontainers/testcontainers-go v0.37.0
 	github.com/testcontainers/testcontainers-go/modules/postgres v0.37.0
--- internal/server/server.go
+++ internal/server/server.go
@@ -7,9 +7,12 @@ import (
 	"strconv"
 	"time"
 
-	_ "github.com/joho/godotenv/autoload"
-
 	"travel-ar-backend/internal/database"
+	"travel-ar-backend/internal/router"
+
+	_ "github.com/joho/godotenv/autoload"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 type Server struct {
@@ -19,17 +22,43 @@ type Server struct {
 }
 
 func NewServer() *http.Server {
-	port, _ := strconv.Atoi(os.Getenv("SERVER_PORT"))
-	NewServer := &Server{
-		port: port,
+	// // 1. 初始化数据库
+	// database.ConnectDatabase()
 
-		db: database.New(),
+	// // 2. 自动迁移
+	// db := database.GetDB()
+	// db.AutoMigrate(
+	// 	&model.Facility{},
+	// 	&model.File{},
+	// 	&model.Notice{},
+	// 	&model.VisitHistory{},
+	// 	&model.Language{},
+	// 	&model.User{},
+	// 	&model.RefreshToken{},
+	// 	&model.Store{},
+	// 	&model.Menu{},
+	// 	&model.Article{},
+	// 	&model.Comment{},
+	// 	&model.Tag{},
+	// 	&model.Tagging{},
+	// )
+
+	// 3. 初始化 Gin 路由
+	r := router.InitRouter()
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	// 4. 端口
+	port := 8080
+	if p := os.Getenv("SERVER_PORT"); p != "" {
+		if v, err := strconv.Atoi(p); err == nil {
+			port = v
+		}
 	}
 
 	// Declare Server config
 	server := &http.Server{
-		Addr:         fmt.Sprintf(":%d", NewServer.port),
-		Handler:      NewServer.RegisterRoutes(),
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      r,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
